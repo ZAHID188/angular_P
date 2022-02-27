197828094956175c7a933812abae60484ce5bc4b
ZAHID188 <zahidpranta5@gmail.com>
2022-02-27 23:09:17 +0600

    some lesson from udemy in forms

--- src/app/forms-angular/forms-module.module.ts
+++ src/app/forms-angular/forms-module.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { FormsAngularComponent } from './forms-angular.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FccFormComponent } from './fcc-form/fcc-form.component';
+import { UdemyFormsLessComponent } from './udemy-forms-less/udemy-forms-less.component';
 
 let component=[
-  FormsAngularComponent,FccFormComponent
+  FormsAngularComponent,FccFormComponent,UdemyFormsLessComponent
 ]
 
 @NgModule({
--- src/app/forms-angular/udemy-forms-less/udemy-forms-less.component.ts
+++ src/app/forms-angular/udemy-forms-less/udemy-forms-less.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-udemy-forms-less',
@@ -10,6 +11,10 @@ export class UdemyFormsLessComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  OnSubmit(form:NgForm){
+console.log(form)
   }
 
 }
