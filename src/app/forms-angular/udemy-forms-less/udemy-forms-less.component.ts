1352caafd12e104bde3500aec380b6627b821d9c
ZAHID188 <zahidpranta5@gmail.com>
2022-03-06 23:51:47 +0600

    Udemy Assignment and forms

--- src/app/forms-angular/udemy-forms-less/udemy-forms-less.component.ts
+++ src/app/forms-angular/udemy-forms-less/udemy-forms-less.component.ts
@@ -4,49 +4,74 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-udemy-forms-less',
   templateUrl: './udemy-forms-less.component.html',
-  styleUrls: ['./udemy-forms-less.component.css']
+  styleUrls: ['./udemy-forms-less.component.css'],
 })
 export class UdemyFormsLessComponent implements OnInit {
-@ViewChild('f') signUpform:NgForm;
+  @ViewChild('f') signUpform: NgForm;
 
-  ngmodelprop="zahid";
-  ans='';
+  ngmodelprop = 'zahid';
+  ans = '';
 
-  constructor() { }
+  // using Form Data
+  Submitted = false;
+  User = {
+    username: '',
+    email: '',
+    secretQuestion: '',
 
-  ngOnInit(): void {
+    gender: '',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  OnSubmit(form: NgForm) {  
+    // console.log(form)
+
+    // using Form Data
+    this.Submitted = true;
+    this.User.username = form.value.userData.username; //form or signupform both is okay.
+    this.User.email = this.signUpform.value.userData.email;
+    this.User.secretQuestion = this.signUpform.value.secret;
+    this.User.gender = this.signUpform.value.gender;
+
+    this.signUpform.reset();
+    // form.reset();  - it's also ok
   }
-
-  OnSubmit(form:NgForm){
-console.log(form)
-
-  }
-
 
   // handling radio button
-  genders=['male','female'];
+  genders = ['male', 'female'];
 
   //setting and patching from values
-  suggestUserName(){
-    const suggestUserName='Super User'
-    // method 1 
+  suggestUserName() {
+    const suggestUserName = 'Super User';
+    // method 1
     // this.signUpform.setValue({
     //   userData:{
     //     username:suggestUserName,
     //     email:'asd@gmail.com',
     //   },
-    //   secret:'pet', 
-    //   gender:'female'    
-    
+    //   secret:'pet',
+    //   gender:'female'
+
     // method 2  -- don't change other inputs
-    this.signUpform.form.patchValue({ 
-      userData:{
-            username:suggestUserName
-        
-          }
-           
+    this.signUpform.form.patchValue({
+      userData: {
+        username: suggestUserName,
+      },
+      secret: 'pet',
+      gender: 'female',
     });
-   
   }
+
+
+//assignment 
+subscriptions=['Basic','Advanced','Pro'];
+selectedSub='Advanced';  // must have to match with the subscriptons values
+@ViewChild ('assignmentForm') formtemp:NgForm;
+onSubmit123(){
+console.log(this.formtemp.value);
+}
 
 }
